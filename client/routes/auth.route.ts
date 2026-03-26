f6c9eeb6ef52c2404c086773e6dc1a405a487214
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-03-26 22:07:40 +0700

    Them tinh nang dang xuat

--- client/routes/auth.route.ts
+++ client/routes/auth.route.ts
@@ -3,5 +3,9 @@ import * as authController from '../controllers/auth.controller';
 
 
 const router = Router();
+
 router.get('/check',authController.checkLogin);
+
+router.get('/logout',authController.logout);
+
 export default router;
