da2c86c84febf2f009931c2961be63c9919c4ff8
Bùi Bùi <huyphuoc09112005@gmail.com>
2026-05-15 15:58:29 +0700

    Them chuc nang up file pdf

--- client/controllers/job.controller.ts
+++ client/controllers/job.controller.ts
@@ -1,74 +1,84 @@
 import { Request, Response } from "express";
 import Job from "../models/job.model";
 import AccountCompany from "../models/account-company.model";
-export const detail=async(req:Request,res:Response)=>{
-   
-   try{
-    const id=req.params.slug;
-    const record =await Job.findOne({
-        _id:id
-    })
-    if(!record){
-        res.json({
-            code:"error",
-            message:"Không tìm thấy công việc!"
+import CV from "../models/cv.model";
+export const detail = async (req: Request, res: Response) => {
+
+    try {
+        const id = req.params.slug;
+        const record = await Job.findOne({
+            _id: id
         })
-        return;
+        if (!record) {
+            res.json({
+                code: "error",
+                message: "Không tìm thấy công việc!"
+            })
+            return;
+        }
+        const jobDetail = {
+            id: record.id,
+            title: record.title,
+            companyId: record.companyId,
+            companyname: "",
+            companyLogo: "",
+            companyModel: "",
+            companyEmployees: "",
+            companyWorkingTime: "",
+            companyWorkOverTime: "",
+            workingTime: "",
+            salaryMin: record.salaryMin,
+            salaryMax: record.salaryMax,
+            description: record.description,
+            images: record.images,
+            position: record.position,
+            workingForm: record.workingForm,
+            companyAddress: "",
+            technologies: record.technologies
+        }
+        const companyInfo = await AccountCompany.findOne({
+            _id: record.companyId
+        })
+        if (companyInfo) {
+            jobDetail.companyname = `${companyInfo.companyName}`;
+            jobDetail.companyLogo = `${companyInfo.logo}`;
+            jobDetail.companyModel = `${companyInfo.companyModel}`;
+            jobDetail.companyEmployees = `${companyInfo.companyEmployees}`;
+            jobDetail.workingTime = `${companyInfo.workingTime}`;
+            jobDetail.companyWorkingTime = `${companyInfo.workingTime}`;
+            jobDetail.companyWorkOverTime = `${companyInfo.workOvertime}`;
+            jobDetail.companyAddress = `${companyInfo.address}`;
+        }
+
+        res.json({
+            code: "success",
+            message: "Thành công!",
+            jobDetail: jobDetail
+        })
+
+
+
+
     }
-    const jobDetail={
-        id:record.id,
-        title:record.title,
-        companyId:record.companyId,
-        companyname:"",
-        companyLogo:"",
-        companyModel:"",
-        companyEmployees:"",
-        companyWorkingTime:"",
-        companyWorkOverTime:"",
-        workingTime:"",
-        salaryMin:record.salaryMin,
-        salaryMax:record.salaryMax,
-        description:record.description,
-        images:record.images,
-        position:record.position,
-        workingForm:record.workingForm,
-        companyAddress:"",
-        technologies:record.technologies
-    }
-    const companyInfo=await AccountCompany.findOne({
-        _id:record.companyId
-    })
-    if(companyInfo){
-        jobDetail.companyname=`${companyInfo.companyName}`;
-        jobDetail.companyLogo=`${companyInfo.logo}`;
-        jobDetail.companyModel=`${companyInfo.companyModel}`;
-        jobDetail.companyEmployees=`${companyInfo.companyEmployees}`;
-        jobDetail.workingTime=`${companyInfo.workingTime}`;
-        jobDetail.companyWorkingTime=`${companyInfo.workingTime}`;
-        jobDetail.companyWorkOverTime=`${companyInfo.workOvertime}`;
-        jobDetail.companyAddress=`${companyInfo.address}`;
+    catch (error) {
+        res.json({
+            code: "error",
+            message: "Có lỗi xảy ra!"
+        })
     }
 
-     res.json({
-        code:"success",
-        message:"Thành công!",
-        jobDetail:jobDetail
-    })
+}
 
-
-
-
-   }
-   catch(error){
+export const applyPost = async (req: Request, res: Response) => {
+    req.body.fileCV = req.file ? req.file.path : "";
+    const newRecord = new CV(req.body);
+    await newRecord.save();
     res.json({
-        code:"error",
-        message:"Có lỗi xảy ra!"
+        code: "success",
+        message: "Ứng tuyển thành công!"
     })
-   }
-   
-   
-   
-   
-   
-   
+
+
+
+
 }
--- client/routes/job.route.ts
+++ client/routes/job.route.ts
@@ -1,7 +1,27 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import * as jobController from '../controllers/job.controller';
 const router = Router();
+import multer from 'multer';
+import { storage } from '../../helpers/cloudinary.helper';
+import * as jobValidate from '../validates/job.validate';
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5 * 1024 * 1024 //5MB
+    }, fileFilter: (req, file, cb) => {
+        if (file.mimetype !== "application/pdf") {
+            cb(null, false);
+            return;
+        }
+        cb(null, true);
+    }
+});
 
-router.get('/detail/:slug',jobController.detail);
+router.get('/detail/:slug', jobController.detail);
+
+router.post('/apply',
+    jobValidate.applyPost,
+    upload.single('fileCV'),
+    jobController.applyPost);
 
 export default router;
